ff5e28e69edf234d9e9aa79fe5663adf6387cbaa
Mohammed El Allali <moal@easj.dk>
2020-09-20 20:49:15 +0200

    Add tutorial

--- Pages/Error.cshtml.cs
+++ Pages/Error.cshtml.cs
@@ -10,7 +10,7 @@ using Microsoft.Extensions.Logging;
 namespace RazorPagesTutorial.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public class ErrorModel : PageModel
+    public class  ErrorModel : PageModel
     {
         public string RequestId { get; set; }
 
